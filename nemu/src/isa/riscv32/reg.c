9160ecf32fadd8f48ddef5a020b6f33359b4fd09
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-12 17:05:14 +0800

    > compile NEMU ysyx_22041403 秦永盛 Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:05:14 up  5:26,  1 user,  load average: 0.48, 0.59, 0.70

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -1,6 +1,6 @@
 #include <isa.h>
 #include "local-include/reg.h"
-
+extern CPU_state cpu ;
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
