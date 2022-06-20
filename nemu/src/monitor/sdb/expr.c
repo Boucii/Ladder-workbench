e8406109285f6a2d391a5b24a05e56863b9d459e
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-21 00:33:03 +0800

    > compile NEMU ysyx_22041403 秦永盛 Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:33:03 up 14:37,  5 users,  load average: 0.16, 0.15, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -94,6 +94,7 @@ static bool make_token(char *e) {
           default: 
 		  tokens[nr_token].type=rules[i].token_type;
                   strncpy(tokens[nr_token].str,substr_start,substr_len);
+                  tokens[nr_token].str[substr_len]='\0';
 		  break;
           case TK_NOTYPE:
 	          nr_token--;
