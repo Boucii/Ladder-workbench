cf1e4c85b4f5f78cc26cf33bfb4fafa175ae1f63
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 00:33:30 +0800

    > compile NEMU ysyx_22041403 秦永盛 Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:33:30 up  2:04,  3 users,  load average: 0.49, 0.38, 0.31

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -23,7 +23,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-#ifdef WATCHPOINT_EN
+#ifdef CONFIG_WATCHPOINT
   int res=difftest();
   if(res==1){
     nemu_state.state=NEMU_STOP;
