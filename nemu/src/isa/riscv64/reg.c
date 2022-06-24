c4901c68427f32f9d0c33d7e1deaf165089bf167
tracer-ysyx2204 <tracer@ysyx.org>
2022-06-24 10:43:01 +0800

    > compile NEMU ysyx_22041403 秦永盛 Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:43:01 up  1:34,  4 users,  load average: 0.23, 0.30, 0.32

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -1,6 +1,6 @@
 #include <isa.h>
 #include "local-include/reg.h"
-
+extern CPU_state cpu;
 const char *regs[] = {
   "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
@@ -17,6 +17,10 @@ void isa_reg_display() {
 
 
 word_t isa_reg_str2val(const char *s, bool *success) {
+  if(strcmp(s,"$pc")==0){
+    *success=true;
+    return (word_t)cpu.pc;
+  }
   *success=false;
   for(int i=0;i<32;i++){
     if(strcmp(s,regs[i])==0){
