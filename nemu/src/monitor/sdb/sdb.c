80b12d1db8f2c897fe88574b21145858bbb34669
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 01:30:51 +0800

    > compile NEMU ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:30:51 up 14:58,  7 users,  load average: 0.59, 0.48, 0.46

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -102,6 +102,9 @@ static int cmd_p(char *args) {
   bool success;
   unsigned int res= expr(arg,&success);
   if(success){
+    if(args[0]=='$'){
+     printf("0x%x\n",res); //is a register
+    }
     printf("%u\n",res);
   }else{
     printf("invalid expression\n");
