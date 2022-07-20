c174a22527ca571e90888f3234ad962a5949d4ac
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-20 15:24:33 +0800

    > compile NEMU ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:24:33 up  4:27,  5 users,  load average: 0.27, 0.41, 0.37

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,24 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+void itoa(unsigned int n, char * buf)
+{
+        int i;
+        
+        if(n < 10)
+        {
+                buf[0] = n + '0';
+                buf[1] = '\0';
+                return;
+        }
+        itoa(n / 10, buf);
 
+        for(i=0; buf[i]!='\0'; i++);
+        
+        buf[i] = (n % 10) + '0';
+        
+        buf[i+1] = '\0';
+}
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -14,7 +31,63 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+    int count=0;
+    char buf[65];
+    char digit[16];
+    memset(buf,0,sizeof(buf));
+    memset(digit,0,sizeof(digit));
+
+    char *str=out;
+
+    va_list ap;
+    va_start(ap, fmt);
+
+    int n;
+    char c;
+    char *s;
+        while(*fmt!='\0'){
+	    if(*fmt=='%'){
+	        fmt++;
+		switch(*fmt){
+			case 'd':{
+			    n=va_arg(ap,int);
+    			    if(n < 0)
+                        {
+                            *str = '-';
+                            str++;
+                            n = -n;
+			    count++;
+                        }
+			    itoa(n,buf);
+			     memcpy(str, buf, strlen(buf));
+			     str += strlen(buf);
+			     count+=strlen(buf);
+			     break;
+				 }
+			case 'c':{
+		 	c = va_arg(ap, int);
+                        *str = c;
+                        str++;
+                        break;
+				 }
+			case 's':{
+                        s = va_arg(ap, char *);
+                        memcpy(str, s, strlen(s));
+                        str += strlen(s);
+			count+=strlen(s);
+                        break;			 
+				 }
+		}
+	    
+	    } else
+        {
+            *str = *fmt;
+            str++;
+        }
+        fmt++;
+	}
+    va_end(ap);
+    return count;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
