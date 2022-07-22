43b9459412cf9cdf0f9598e5d1533d7541c4a788
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-22 20:41:38 +0800

    > compile NEMU ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:41:38 up  9:09,  5 users,  load average: 0.16, 0.26, 0.31

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -1,7 +1,28 @@
 #include <common.h>
-
+#include <iringbuf/iringbuf.h>
+#include<cpu/decode.h>
 extern uint64_t g_nr_guest_inst;
 FILE *log_fp = NULL;
+
+char iringbuf[20][128];
+
+uint32_t ibufptr=0;
+
+int write_irbuf(Decode *d){
+  strcpy(iringbuf[ibufptr],d->logbuf);
+  ibufptr++;
+  if(!(ibufptr<20)){
+    ibufptr=0;
+  }
+  return 0;
+}
+void print_buf(){
+  int i=0;
+  for(;i<ibufptr-1;i++){
+	log_write("     %s\n",iringbuf[i]);
+  }
+  log_write("---->%s\n",iringbuf[i]);
+}
 
 void init_log(const char *log_file) {
   log_fp = stdout;
