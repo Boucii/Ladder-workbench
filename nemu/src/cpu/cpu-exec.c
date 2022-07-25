15d5eb8184fb2a8d0dade821c85e0997e0e37da9
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 01:13:10 +0800

    > compile NEMU ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:13:10 up 13:39,  3 users,  load average: 0.47, 0.27, 0.21

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -131,13 +131,13 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
-#ifdef CPNFIG_FTRACE
+#ifdef CONFIG_FTRACE
 	      ftrace_display();
 #endif
       if(nemu_state.state == NEMU_ABORT||nemu_state.halt_ret != 0){
       log_write("--------Abnormal End of Simulation---------\n");
 	      print_buf();
-#ifdef CPNFIG_FTRACE
+#ifdef CONFIG_FTRACE
 	      ftrace_display();
 #endif
       }
