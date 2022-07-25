4cd7577f764a285e19e045e9d2548025a8acbc2e
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-26 01:03:37 +0800

    > compile NEMU ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:03:37 up 13:29,  3 users,  load average: 0.13, 0.16, 0.18

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -7,7 +7,7 @@ static struct func{
 	paddr_t begin_addr;
 	paddr_t end_addr;
 	uint32_t size;
-	char name[100];
+	char name[200];
 } func_table[1024];
 
 static short func_idx=0;
@@ -43,6 +43,7 @@ static void tableheader(const char *pbuff)
 					func_table[func_idx].begin_addr = psym->st_value;
 					func_table[func_idx].end_addr = psym->st_value+psym->st_size-4;
 					func_table[func_idx].size = psym->st_size;
+					
 					strcpy(func_table[func_idx].name, psym->st_name+pbuffstr);
 					func_idx++;
 				}	
