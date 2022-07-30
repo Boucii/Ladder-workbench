4a02e478e84dbfde8ab0492e77be861f3ddaf2ff
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 22:15:11 +0800

    > compile NEMU ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:15:11 up  9:11,  3 users,  load average: 0.24, 0.22, 0.27

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -23,71 +23,78 @@ void itoa(unsigned int n, char * buf)
         buf[i+1] = '\0';
 }
 int printf(const char *fmt, ...) {
-  panic("Not implemented");
+  char buf[65536];
+  va_list args;
+  va_start(args,fmt);
+  int res=vsprintf(buf,fmt,args);
+  va_end(args);
+  putstr(buf);
+  free(buf);
+  return res;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+	int count=0;
+        char buf[65];
+        char digit[16];
+        memset(buf,0,sizeof(buf));
+        memset(digit,0,sizeof(digit));
+                                                                
+        char *str=out;
+                                                                
+        int n;
+        char c;
+        char *s;
+            while(*fmt!='\0'){
+                if(*fmt=='%'){
+                    fmt++;
+            	switch(*fmt){
+            		case 'd':{
+            		    n=va_arg(ap,int);
+        			    if(n < 0)
+                            {
+                                *str = '-';
+                                str++;
+                                n = -n;
+            		    count++;
+                            }
+            		    itoa(n,buf);
+            		     memcpy(str, buf, strlen(buf));
+            		     str += strlen(buf);
+            		     count+=strlen(buf);
+            		     break;
+            			 }
+            		case 'c':{
+            	 	c = va_arg(ap, int);
+                            *str = c;
+                            str++;
+                            break;
+            			 }
+            		case 's':{
+                            s = va_arg(ap, char *);
+                            memcpy(str, s, strlen(s));
+                            str += strlen(s);
+            		count+=strlen(s);
+                            break;			 
+            			 }
+                    }
+            }else{
+            	    *str = *fmt;
+                        str++;
+            }
+            fmt++;
+            }
+                                                                
+        *str='\0';
+        return count;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-    int count=0;
-    char buf[65];
-    char digit[16];
-    memset(buf,0,sizeof(buf));
-    memset(digit,0,sizeof(digit));
-
-    char *str=out;
-
-    va_list ap;
-    va_start(ap, fmt);
-
-    int n;
-    char c;
-    char *s;
-        while(*fmt!='\0'){
-	    if(*fmt=='%'){
-	        fmt++;
-		switch(*fmt){
-			case 'd':{
-			    n=va_arg(ap,int);
-    			    if(n < 0)
-                        {
-                            *str = '-';
-                            str++;
-                            n = -n;
-			    count++;
-                        }
-			    itoa(n,buf);
-			     memcpy(str, buf, strlen(buf));
-			     str += strlen(buf);
-			     count+=strlen(buf);
-			     break;
-				 }
-			case 'c':{
-		 	c = va_arg(ap, int);
-                        *str = c;
-                        str++;
-                        break;
-				 }
-			case 's':{
-                        s = va_arg(ap, char *);
-                        memcpy(str, s, strlen(s));
-                        str += strlen(s);
-			count+=strlen(s);
-                        break;			 
-				 }
-	        }
-        }else{
-		    *str = *fmt;
-	            str++;
-	}
-        fmt++;
-	}
-
-    *str='\0';
-    va_end(ap);
-    return count;
+     va_list ap;
+     va_start(ap, fmt);
+     int res=vsprintf(out,fmt,ap);
+     va_end(ap);
+     return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
