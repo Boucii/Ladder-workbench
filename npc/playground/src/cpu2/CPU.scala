d0150b62ef0374f127f66d46c1c5224eebb44a86
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-27 20:47:16 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:47:16 up 10:41,  3 users,  load average: 0.66, 0.42, 0.27

--- npc/playground/src/cpu2/CPU.scala
+++ npc/playground/src/cpu2/CPU.scala
@@ -555,16 +555,16 @@ when(pt3==="b011".U && pt5==="b01000".U && pt6==="b11".U){    // sd
   dpc:=Mux((src1.asUInt=/=src2.asUInt),pc,pc+dest)
 }.elsewhen(pt5==="b11011".U && pt6==="b11".U){    // jal    
   src1:=immJ
-  regs.io.wdata:=pc+4
+  regs.io.wdata:=pc+4.U
   regs.io.waddr:=dest
   regs.io.wen:=1.U
   dpc:=pc+src1
 }.elsewhen(pt0==="b0000000".U && pt1==="b00000".U && pt2==="b00000".U && pt3==="b000".U && pt4==="b00000".U && pt5==="b11100".U && pt6==="b11".U){    // ecall  
 }.elsewhen(pt0==="b0000000".U && pt1==="b00001".U && pt2==="b00000".U && pt3==="b000".U && pt4==="b00000".U && pt5==="b11100".U && pt6==="b11".U){    // ebreak 
-  stopflag1:=1.U
+  stopflag:=1.U
 }.otherwise{    // inv 
   stopflag:=1.U
 }
   //update pc reg
-  pc:=Mux(stopflag===1.U,dnpc,pc)
+  pc:=Mux(stopflag===1.U,dpc,pc)
 }
