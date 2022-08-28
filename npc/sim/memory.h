25de010e46b253b3b8963e452c04119e18abc302
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-28 18:39:38 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:39:38 up  9:19,  2 users,  load average: 0.33, 0.31, 0.24

--- npc/sim/memory.h
+++ npc/sim/memory.h
@@ -4,6 +4,6 @@
 #include<cassert>
 int mem_init();
 uint64_t pmem_read(int addr);
-int pmem_write(uint32_t content,uint32_t addr,uint32_t len);
+int pmem_write(uint64_t content,uint64_t addr,uint32_t len);
 int free_memory();
 long load_img();
