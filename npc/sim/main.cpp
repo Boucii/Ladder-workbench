175f6da76fa4efad237568951c4b40f1096ac24b
tracer-ysyx2204 <tracer@ysyx.org>
2022-08-31 10:03:59 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:03:59 up 44 min,  3 users,  load average: 0.79, 0.69, 0.52

--- npc/sim/main.cpp
+++ npc/sim/main.cpp
@@ -54,7 +54,7 @@ void reset(int n) {
 
 int main(int argc, char** argv, char** env){
   cout<<argc<<endl;
-  cout<<"argvs:"<<argv[0]<<argv[1]<<argv[2]<<argv[3]<<endl;
+  cout<<"argvs:"<<argv[0]<<argv[1]<<endl;
   contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
   top= new VTOP{contextp};
