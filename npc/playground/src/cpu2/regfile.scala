4588d22535ae831d025417e633b40c73164acadc
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-01 13:33:42 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:33:42 up  1:00,  4 users,  load average: 0.20, 0.23, 0.29

--- npc/playground/src/cpu2/regfile.scala
+++ npc/playground/src/cpu2/regfile.scala
@@ -18,7 +18,7 @@ class Regfile extends Module{
   })
   val regs=Reg(Vec(32,UInt(64.W)))
   for (i<-0 to 32-1) {
-      io.regsout:=regs(i)
+      io.regsout(i):=regs(i)
   }
   val data1=Mux((io.raddr1===0.U),0.U,regs(io.raddr1))
   val data2=Mux((io.raddr2===0.U),0.U,regs(io.raddr2))
