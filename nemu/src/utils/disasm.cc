ff307079fedc38b8e196c9dff40fd272a9ab9943
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-02 20:52:02 +0800

    > compile NEMU ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:52:02 up  1:42,  1 user,  load average: 0.65, 0.36, 0.36

--- nemu/src/utils/disasm.cc
+++ nemu/src/utils/disasm.cc
@@ -2,12 +2,15 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 #endif
-
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCDisassembler/MCDisassembler.h"
 #include "llvm/MC/MCInstPrinter.h"
+#if LLVM_VERSION_MAJOR >= 14
+#include "llvm/MC/TargetRegistry.h"
+#else
 #include "llvm/Support/TargetRegistry.h"
+#endif
 #include "llvm/Support/TargetSelect.h"
 
 #if defined(__GNUC__) && !defined(__clang__)
