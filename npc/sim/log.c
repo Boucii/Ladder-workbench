81c6bed0d14904a3a9c9e0926cb094a9182cf8a9
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 00:29:44 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:29:44 up  5:19,  1 user,  load average: 0.37, 0.27, 0.26

---
+++ npc/sim/log.c
@@ -0,0 +1,14 @@
+#include "log.h"
+#define Log(format, ...) \
+    _Log(ANSI_FMT("[%s:%d %s] " format, ANSI_FG_BLUE) "\n", \
+        __FILE__, __LINE__, __func__, ## __VA_ARGS__)
+
+char *log_fp="../build/log/npc_log.txt";
+void LogInit(){
+  if(log_file!=NULL){
+    FILE *fp = fopen(log_file, "w");
+    Assert(fp, "Can not open '%s'", log_file);
+    log_fp = fp;
+  }
+  Log("Log is written to %s", log_file ? log_file : log_fp);
+}
---
+++ npc/sim/log.h
@@ -0,0 +1,1 @@
+void LogInit()
