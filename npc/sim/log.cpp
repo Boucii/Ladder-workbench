f74f3f60e05f86aecdc661adf560e15bbb3650a3
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 12:29:06 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:29:06 up  2:33,  3 users,  load average: 0.19, 0.25, 0.32

--- npc/sim/log.cpp
+++ npc/sim/log.cpp
@@ -1,9 +1,10 @@
 #include "log.h"
+
 ofstream fout("../build/log/npc_log.txt",ios::out);
+
 void LogInit(){
   assert(fout.is_open());
 }
 void Log(string content){
     fout<<content<<endl;
 }
-
--- npc/sim/log.h
+++ npc/sim/log.h
@@ -6,4 +6,5 @@
 
 using namespace std;
 void Log(string content);
-void LogInit()
+void LogInit();
+
