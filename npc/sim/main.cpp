842ada2f45595dc47d96565d32781289ed3322e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-04 11:47:25 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:47:25 up  1:39,  3 users,  load average: 1.01, 0.56, 0.47

--- npc/sim/main.cpp
+++ npc/sim/main.cpp
@@ -109,7 +109,7 @@ int main(int argc, char** argv, char** env){
     uint32_t cur_inst = (uint32_t)pmem_read(addr);
     top->io_InstIn = cur_inst;
     uint8_t *instaddr=(uint8_t *)&cur_inst;
-    disassemble(logbuf, 90,addr, instaddr, 4);
+    disassemble(logbuf, 90, (int64_t)addr, instaddr, 4);
     string temp=logbuf;
     Log(temp);
     Log("\n");
