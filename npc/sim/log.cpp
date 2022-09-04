a124ce9852951a974ab6acc64fb95d90901429fa
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-04 16:25:37 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:25:37 up  1:24,  1 user,  load average: 0.37, 0.24, 0.31

--- npc/sim/log.cpp
+++ npc/sim/log.cpp
@@ -1,13 +1,11 @@
 #include "log.h"
-
 ofstream fout("./build/npc_log.txt",ios::out|ios::trunc);
 
 void LogInit(){
-  cout<<"Current path is:"<<getcwd(NULL,0)<<'\n';
-  assert(fout.is_open()&&fout);
-  cout<<"log file is open now\n";
+    assert(fout.is_open()&&fout);
 }
 void Log(string content){
     fout<<content;
-    sync();
+    fout.flush();
+
 }
