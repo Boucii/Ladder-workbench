e8587400504da9bf6b9aa750a80067b335190a41
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-05 18:15:00 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:15:00 up  2:27,  1 user,  load average: 0.43, 0.65, 0.40

--- npc/playground/src/cpu2/CPU.scala
+++ npc/playground/src/cpu2/CPU.scala
@@ -99,7 +99,7 @@ class TOP extends Module{
   val immS=Cat(Mux(inst(31)=/=1.U,inst(31,25),Cat(0xff.U,inst(31,25))),inst(11,7))
   val immU=inst(31,12)<<12
   //val immJ=Cat(Cat(Cat(Mux(inst(31)===1.U,Cat(0xff.U,inst(31,31)),inst(31,31))<<20,inst(19,12)<<12),inst(20,20)<<11),inst(30,21)<<1) //optimization?
-  val immJ=Mux(inst(31)===1.U,Cat(0xff.U,inst(31,31)),inst(31,31))<<20+inst(19,12)<<12)+inst(20,20)<<11+inst(30,21)<<1 //optimization?
+  val immJ=Mux(inst(31)===1.U,Cat(0xff.U,inst(31,31)),inst(31,31))<<20+inst(19,12)<<12+inst(20,20)<<11+inst(30,21)<<1 //optimization?
   val immB=Mux(inst(31)=/=1.U,inst(31,31),Cat(0xff.U,inst(31,31)))<<12+inst(7,7)<<11+inst(30,25)<<5+inst(11,8)<<1
   //decode to src and dest
   val dest=Wire(UInt(64.W))
