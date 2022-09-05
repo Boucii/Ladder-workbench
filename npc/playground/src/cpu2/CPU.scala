5460231aed080818f6a5fc38fbf342b1fcda3361
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-05 11:57:46 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:57:46 up 25 min,  1 user,  load average: 0.60, 0.39, 0.27

--- npc/playground/src/cpu2/CPU.scala
+++ npc/playground/src/cpu2/CPU.scala
@@ -103,7 +103,7 @@ class TOP extends Module{
   src2:=0.U
   dest:=rd//default is rd,todo and change
 
-  printf(“\n\n\nimmj = %d\n\n\n”, immJ);
+  printf(“\n\n\nimmj = \n\n\n”);
   //itermidiate
   val intermediate=Wire(UInt(32.W))
   intermediate:=0.U
