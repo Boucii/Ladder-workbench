1d648e423594b88c91687d1314af0fc10525d261
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-05 14:17:23 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:17:23 up  2:06,  1 user,  load average: 0.24, 0.42, 0.39

--- npc/playground/src/cpu2/CPU.scala
+++ npc/playground/src/cpu2/CPU.scala
@@ -92,12 +92,7 @@ class TOP extends Module{
   val immU=inst(31,12)<<12
   val immJ=Cat(Cat(Cat(Mux(inst(31)===1.U,Cat(0xff.U,inst(31,31)),inst(31,31))<<20,inst(19,12)<<12),inst(20,20)<<11),inst(30,21)<<1) //optimization?
   val immB=Cat(Cat(Cat(Mux(inst(31)=/=1.U,inst(31,31),Cat(0xff.U,inst(31,31)))<<12,inst(7,7)<<11),inst(30,25)<<5),inst(11,8)<<1)
-/*
-  val immJ=Wire(UInt(64.W))
-  immJ:=0.U
-  printf(p"$inst \n")
-  immJ:=Cat(Cat(Cat(Mux(inst(31)===1.U,Cat(0xff.U,inst(31,31)),inst(31,31))<<20,inst(19,12)<<12),inst(20,20)<<11),inst(30,21)<<1)
-*/
+
   //decode to src and dest
   val dest=Wire(UInt(64.W))
   val src1=Wire(UInt(64.W))
@@ -613,7 +608,7 @@ when(pt3==="b011".U && pt5==="b01000".U && pt6==="b11".U){    // sd
   dpc:=Mux((src1.asUInt===src2.asUInt),pc,pc+dest)
 }.elsewhen(pt5==="b11011".U && pt6==="b11".U){    // jal    
   src1:=immJ
-  printf(p"immJ=0x${Hexadecimal(src1)} \n")
+  //printf(p"immJ=0x${Hexadecimal(src1)} \n")
   //println(s"\nimmJ= $immJ")
   regs.io.wdata:=pc+4.U
   regs.io.waddr:=dest
