b66cb042f8d7bcd3fb2318babae339ffc9322020
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-05 12:59:38 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.11.0-41-generic #45~20.04.1-Ubuntu SMP Wed Nov 10 10:20:10 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:59:38 up 48 min,  1 user,  load average: 0.71, 0.60, 0.38

--- npc/playground/test/src/cpu/CpuTest.scala
+++ npc/playground/test/src/cpu/CpuTest.scala
@@ -6,7 +6,7 @@ import chisel3._
 object GenVerilog extends App {
   //(new chisel3.stage.ChiselStage).emitVerilog(new TOP)
 //(new chisel3.stage.ChiselStage).execute(args, Seq(chisel3.stage.ChiselGeneratorAnnotation(() => new TOP())))
-    (new chisel3.stage.ChiselStage).emitVerilog(new TOP, args)
+    (new chisel3.stage.ChiselStage).emitVerilog(new TOP(), args)
 }
 /*
 final def GenVerilog(gen:=>RawModule,args:Array[String]=A){
