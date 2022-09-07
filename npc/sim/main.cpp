6cfdf7d3e9efb3078029d577877dba57422e76ad
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-07 15:03:43 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:03:43 up  2:47,  1 user,  load average: 0.06, 0.20, 0.24

--- npc/sim/main.cpp
+++ npc/sim/main.cpp
@@ -78,6 +78,7 @@ enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
 void (*ref_difftest_memcpy)(uint32_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
+void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 void init_difftest() {
   char ref_so_file[]="/home/mint/ysyx-workbench/nemu/build/riscv64-nemu-interpreter-so";
