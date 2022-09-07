028c069cf3c618c5c9238b0e7a51a0306a2de661
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-07 14:52:41 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:52:41 up  2:36,  1 user,  load average: 0.25, 0.34, 0.26

--- npc/sim/main.cpp
+++ npc/sim/main.cpp
@@ -84,22 +84,25 @@ void init_difftest() {
   handle = dlopen(ref_so_file, RTLD_LAZY);
   assert(handle);
 
-  ref_difftest_memcpy = dlsym(handle, "difftest_memcpy");
+  ref_difftest_memcpy = (void (*)(uint32_t addr, void *buf, size_t n, bool direction))dlsym(handle, "difftest_memcpy");
   assert(ref_difftest_memcpy);
 
-  ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
+  ref_difftest_regcpy = (void (*)(void *dut, bool direction))(dlsym(handle, "difftest_regcpy"));
+  //ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
   assert(ref_difftest_regcpy);
 
-  ref_difftest_exec = dlsym(handle, "difftest_exec");
+  ref_difftest_exec = (void (*)(uint64_t n))(dlsym(handle, "difftest_exec"));
+  //ref_difftest_exec = dlsym(handle, "difftest_exec");
   assert(ref_difftest_exec);
 
-  ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
+  ref_difftest_raise_intr = (void (*)(uint64_t NO))(dlsym(handle, "difftest_raise_intr"));
+  //ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
   assert(ref_difftest_raise_intr);
 
-  void (*ref_difftest_init)(int) = dlsym(handle, "difftest_init");
+  void (*ref_difftest_init)(int) = (void (*)(int))dlsym(handle, "difftest_init");
   assert(ref_difftest_init);
 
-  ref_difftest_init();
+  ref_difftest_init(0);
   ref_difftest_memcpy(RESET_VECTOR,mem, img_size, DIFFTEST_TO_REF);
   ref_difftest_regcpy(cpu_gpr, DIFFTEST_TO_REF);
 }
