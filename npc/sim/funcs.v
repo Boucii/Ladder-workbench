4fc5c8501807d2c9691beab2ad743c9b6884b56a
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-13 09:24:17 +0800

    > sim RTL ysyx_22041413 QinYongSheng Linux linux-lab-host 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:24:17 up 30 min,  1 user,  load average: 0.31, 0.26, 0.23

--- npc/sim/funcs.v
+++ npc/sim/funcs.v
@@ -25,11 +25,9 @@ import "DPI-C" function void pmem_read_dpi(
 import "DPI-C" function void pmem_write_dpi(
   input longint waddr, input longint wdata, input byte wmask);
 wire [63:0] rdata_in;
-assign rdata=rdata_in;
 always @(*) begin
   pmem_read_dpi(raddr, rdata_in);
   pmem_write_dpi(waddr, wdata, wmask);
 end
-
-
+assign rdata=rdata_in;
 endmodule
