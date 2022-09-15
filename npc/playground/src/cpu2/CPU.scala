1a2b03a73f1403363ebcc568a8525e69cb824977
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-15 12:46:55 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:46:55 up 44 min,  1 user,  load average: 0.21, 0.12, 0.14

--- npc/playground/src/cpu2/CPU.scala
+++ npc/playground/src/cpu2/CPU.scala
@@ -315,16 +315,20 @@ when(pt3==="b011".U && pt5==="b01000".U && pt6==="b11".U){    // sd
 }.elsewhen(pt0==="b0000001".U && pt3==="b110".U && pt5==="b01110".U && pt6==="b11".U){    // remw   
   regs.io.raddr1:=rs1
   regs.io.raddr2:=rs2
-  intermediate:=(src1(31,0).asSInt%src2(31,0).asSInt).asUInt
-  regs.io.wdata:=Mux(intermediate(31)=/=1.U,intermediate(31,0),Cat(0xffffffffL.U,intermediate(31,0)))
   src1:=regs.io.rdata1
   src2:=regs.io.rdata2
+  intermediate:=(src1(31,0).asSInt%src2(31,0).asSInt).asUInt
+  regs.io.wdata:=Mux(intermediate(31)=/=1.U,intermediate(31,0),Cat(0xffffffffL.U,intermediate(31,0)))
+  regs.io.waddr:=dest 
+  regs.io.wen:=1.U    
 }.elsewhen(pt0==="b0000001".U && pt3==="b111".U && pt5==="b01110".U && pt6==="b11".U){    // remuw  
   regs.io.raddr1:=rs1
   regs.io.raddr2:=rs2
-  regs.io.wdata:=src1%src2
   src1:=regs.io.rdata1
   src2:=regs.io.rdata2
+  regs.io.wdata:=src1%src2
+  regs.io.waddr:=dest 
+  regs.io.wen:=1.U    
 }.elsewhen(pt0==="b0000001".U && pt3==="b111".U && pt5==="b01100".U && pt6==="b11".U){    // remu   
   regs.io.raddr1:=rs1
   regs.io.raddr2:=rs2
