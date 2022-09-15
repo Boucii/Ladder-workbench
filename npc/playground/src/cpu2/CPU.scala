6a3a7a2aff09bfcc5b41eb7cf73dc61802ee5ce2
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-15 12:54:35 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:54:35 up 52 min,  1 user,  load average: 1.13, 0.40, 0.25

--- npc/playground/src/cpu2/CPU.scala
+++ npc/playground/src/cpu2/CPU.scala
@@ -556,7 +556,8 @@ when(pt3==="b011".U && pt5==="b01000".U && pt6==="b11".U){    // sd
   regs.io.raddr1:=rs1
   src1:=regs.io.rdata1
   src2:=immI
-  regs.io.wdata:=(src1(31,0).asSInt>>src2(4,0)).asUInt
+  intermediate:=(src1(31,0).asSInt>>src2(4,0)).asUInt
+  regs.io.wdata:=Mux(intermediate(31)===1.U,Cat(0xffffffffL.U,intermediate(31,0)),intermediate(31,0))
   regs.io.waddr:=dest
   regs.io.wen:=1.U
 }.elsewhen(pt0==="b0000000".U && pt3==="b101".U && pt5==="b00100".U && pt6==="b11".U){    // srli   
