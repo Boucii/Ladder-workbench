de708f7d68c92f0dfb4c817b4679aa7db9d237a0
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-19 00:24:14 +0800

    > generate verilog ysyx_22041413 QinYongSheng Linux linux-lab-host 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:24:14 up  2:50,  1 user,  load average: 0.19, 0.20, 0.23

--- npc/playground/src/cpu2/CPU.scala
+++ npc/playground/src/cpu2/CPU.scala
@@ -394,7 +394,7 @@ when(pt3==="b011".U && pt5==="b01000".U && pt6==="b11".U){    // sd
   regs.io.raddr2:=rs2
   src1:=regs.io.rdata1
   src2:=regs.io.rdata2
-  intermediate(32,0):=(Cat(src1(31),src1).asSInt/Cat(src2(src2),src2.asSInt)).asUInt
+  intermediate(63,0):=((Cat(src1(63),src1)/Cat(src2(63),src2))(63,0))(63,0)
   regs.io.wdata:=intermediate(31,0)
   regs.io.waddr:=dest
   regs.io.wen:=1.U
@@ -412,7 +412,7 @@ when(pt3==="b011".U && pt5==="b01000".U && pt6==="b11".U){    // sd
   regs.io.raddr2:=rs2
   src1:=regs.io.rdata1
   src2:=regs.io.rdata2
-  intermediate(32,0):=(Cat(src1(31),src1).asUInt/Cat(src2(src2),src2.asUInt)).asUInt
+  intermediate(32,0):=(Cat(src1(31),src1).asUInt/Cat(src2(31),src2.asUInt)).asUInt
   regs.io.wdata:=Mux(intermediate(31)=/=1.U,intermediate(31,0),Cat(0xffffffffL.U,intermediate(31,0)))
   regs.io.waddr:=dest
   regs.io.wen:=1.U
@@ -421,7 +421,7 @@ when(pt3==="b011".U && pt5==="b01000".U && pt6==="b11".U){    // sd
   regs.io.raddr2:=rs2
   src1:=regs.io.rdata1
   src2:=regs.io.rdata2
-  intermediate(32,0):=(Cat(src1(31),src1).asSInt/Cat(src2(src2),src2.asSInt)).asUInt
+  intermediate(32,0):=(Cat(src1(31),src1).asSInt/Cat(src2(31),src2.asSInt)).asUInt
   regs.io.wdata:=Mux(intermediate(31)=/=1.U,intermediate(31,0),Cat(0xffffffffL.U,intermediate(31,0)))
   regs.io.waddr:=dest
   regs.io.wen:=1.U
